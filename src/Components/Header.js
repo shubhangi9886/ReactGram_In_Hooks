093dfa2bb923c995e0034fa4844e0f6e51f771ac
komalahire <komala18@navgurukul.org>
2020-10-21 18:35:07 +0530

    updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Reactgram() {
     return (
         <p
-            style={{ fontSize: '50px', color: '#125688', fontFamily: "'Cookie', cursive", }}>
+            style={{ fontSize: '50px', color: '#125688', fontFamily: 'sans-serif' }}>
             Reactgram</p>
     )
 }
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,44 +1,68 @@
-import React, { useState, } from 'react';
-import { Button, Typography, Grid } from '@material-ui/core';
+import React, { useState, Component, } from 'react';
+import { Button, Typography, Card, CardMedia, CardContent, } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ForumIcon from '@material-ui/icons/Forum';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import posts from '../Data/Post-data';
 import { makeStyles } from '@material-ui/core/styles'
 
-
 const useStyles = makeStyles(theme => ({
     root: {
-        flexGrow: 1,
-        padding: theme.spacing(10),
-    }
+        display: 'grid',
+        gridTemplateColumns: '22.5% 22.5% 22.5%',
+        justifyContent: 'center',
+        padding: '80px',
+    },
+
+    card: {
+        maxWidth: 445,
+        display: 'grid',
+        margin: '10px',
+    },
+
+    button: {
+        color: '#5e47e0',
+        width: '30%',
+        marginTop: 20,
+    },
+
+    com_button: {
+        color: '#5e47e0',
+        width: '30%',
+        marginTop: 20,
+        marginLeft: 20,
+    },
+
+    media: {
+        height: '500px',
+    },
 }))
 
-function Post(props) {
-    const [post, setPost] = useState(0);
-    // console.log(post)
-
-    // function increment() {
-    //     setPost(count => count + 1)
-    // }
-
+function Post() {
     const classes = useStyles()
-
     return (
-        <div className={classes.root} >
-            <Grid container spacing={2} direction="row" justify='' alignItems="flex-start">
-                <Grid item xs={12}  col={5}>
-                    {posts.map(post => (
-                        <Grid key={post.id} style={{ height: 530, width: '21%', border: '1px solid black', marginLeft: '30%', marginTop: '2%', }}>
-                            <img src={post.Image} style={{ width: "95%", height: "350px", marginTop: '2%' }} alt="post" />
-                            <Typography style={{ textAlign: 'center', marginTop: '2%' }}>{post.caption}</Typography>
-                            <Button style={{ height: 50, width: '35%', border: '1px solid black', marginTop: '10%' }}  > <FavoriteIcon style={{ color: '#5e47e0', width: '30%', height: '10%' }} /></Button>
-                            <Button style={{ height: 50, width: '35%', border: '1px solid black', marginTop: '10%', marginLeft: '20%' }} > <ForumIcon style={{ color: '#5e47e0', width: '30%', height: '10%' }} /> </Button>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
+        <div className={classes.root}>
+            {posts.map((post, i) => (
+                <Card key={i} className={classes.card}>
+                    <CardMedia className={classes.media} image={post.Image} />
+                    <CardContent>
+                        <Typography className={classes.text} variant="body2" color="textSecondary" component="p">
+                            {post.caption}
+                        </Typography>
+                        <Button variant="outlined" className={classes.button}>
+                            <FavoriteIcon />
+                            {/* {count.like} */}
+                        </Button>
+                        <Button variant="outlined" className={classes.com_button}>
+                            <QuestionAnswerIcon />
+                        </Button>
+                    </CardContent>
+                </Card>
+            )
+            )}
         </div>
     )
+
+
 }
 
 export default Post;
